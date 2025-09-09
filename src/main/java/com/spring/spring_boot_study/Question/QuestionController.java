0630c9635fc54de7865bdf12983831d7b49e1599
kimeunseokk <rpqhfls2whdk@gmail.com>
2025-09-09 17:15:22 +0900

    2025.0909

--- src/main/java/com/spring/spring_boot_study/Question/QuestionController.java
+++ src/main/java/com/spring/spring_boot_study/Question/QuestionController.java
@@ -6,12 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor // final이 붙은 속성을 포함하는 생성자를 자동으로 만들어주는 역할
 @Controller // Rest api처리하는 컨트롤러임을 알려줌
 public class QuestionController {
@@ -19,15 +21,14 @@ public class QuestionController {
     private final QuestionRepository questionRepository;
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
-   
+    @GetMapping("/list")
     public String list(Model model) { // Model을 지정하면 객체가자동으로 생성된다.
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
 
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model,@PathVariable("id") Integer id) {
         return "question_detail";
     }
